e7eacb07f182ed1fe6200c103eac284001cf1fed
TheRootDaemon <harshath3018@gmail.com>
2025-09-08 08:21:18 +0530

    init: Bare Bones folio, with a hero section

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import favicon from "$lib/assets/favicon.svg";
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+{@render children?.()}
+
+<style>
+	:global(body) {
+		padding: 0;
+		margin: 0;
+		background-color: #232136;
+	}
+</style>
