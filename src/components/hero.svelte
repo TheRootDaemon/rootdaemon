26f0590bcbdbdecf6dd43530920dc86ce16f3a77
TheRootDaemon <harshath3018@gmail.com>
2025-09-14 22:09:23 +0530

    refactor: Hero component added styles

---
+++ src/components/hero.svelte
@@ -0,0 +1,52 @@
+<section class="hero">
+    <div><p id="whoami">Harshavardhan, aka The Root Daemon</p></div>
+    <div id="man">
+        <p>Developer, obsessed with clean workflows and cleaner code.</p>
+    </div>
+    <div id="daemon"><p>Oh, let’s talk about Root Daemons...</p></div>
+    <div class="desc">
+        <p>Root Daemons are Privileged processes running in the Shadows...</p>
+    </div>
+    <div class="desc">
+        <p>
+            Those Daemons has Absolute Powers of course with Responsibilities...
+        </p>
+    </div>
+</section>
+
+<style>
+    p {
+        margin: 0;
+        font-weight: bold;
+    }
+
+    .hero {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-family: Outfit;
+        background-color: white;
+        width: clamp(10vw, 90%, 1100px);
+        margin-inline: auto;
+    }
+
+    .hero {
+        flex: 1 1 auto;
+    }
+
+    #whoami {
+        font-size: 2rem;
+    }
+
+    #man {
+        font-size: 1.8rem;
+    }
+
+    #daemon {
+        font-size: 1.5rem;
+    }
+
+    .desc {
+        font-size: 1.3rem;
+    }
+</style>
